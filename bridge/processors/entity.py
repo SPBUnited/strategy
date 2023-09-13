063766abc894a9dcf8df0dc784fb50689b4a2e70
Arseniy <yarmolinskiyam@gmail.com>
2023-09-13 14:50:11 +0300

    entity

---
+++ bridge/processors/entity.py
@@ -0,0 +1,57 @@
+"""
+Структура для хранения информации об одном объекте на поле (робот или мяч)
+
+Хранит:
+- положение
+- скорость
+- ускорение
+- угол
+- радиус
+"""
+
+import math
+import bridge.processors.auxiliary as aux
+
+class Entity:
+
+    """
+    Конструктор
+    
+    @param pos Изначальное положение объекта. Тип: aux.Point
+    @param angle Угол поворота объекта [рад]
+    @param R Радиус объекта [м]
+    """
+    def __init__(self, pos, angle, R) -> None:
+        self.pos = pos
+        self.vel = aux.Point(0, 0)
+        self.acc = aux.Point(0, 0)
+        self.angle = angle
+        self.R = R
+
+    """
+    Обновить положение и рассчитать исходя из этого скорость и ускорение
+
+    @todo Реализовать расчет скоростей и ускорений
+    """
+    def update(self, pos):
+        self.pos = pos
+
+    """ Геттер положения """
+    def getPos(self) -> aux.Point:
+        return self.pos
+    
+    """ Геттер скорости """
+    def getVel(self) -> aux.Point:
+        return self.vel
+    
+    """ Геттер ускорения """
+    def getAcc(self) -> aux.Point:
+        return self.acc
+    
+    """ Геттер угла """
+    def getR(self) -> float:
+        return self.angle
+    
+    """ Геттер радиуса """
+    def getR(self) -> float:
+        return self.R
