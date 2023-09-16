1966125b15580b6789f44f682014976895c90ca1
Arseniy <yarmolinskiyam@gmail.com>
2023-09-16 21:10:48 +0300

    vector field

--- bridge/processors/strategy.py
+++ bridge/processors/strategy.py
@@ -7,17 +7,42 @@ import bridge.processors.waypoint as wp
 import bridge.processors.const as const
 import bridge.processors.auxiliary as aux
 import math
+from enum import Enum
+
+class States(Enum):
+    DEBUG = 0
+    DEFENCE = 1
 
 class Strategy:
     def __init__(self) -> None:
-        pass
+        self.state = States.DEBUG
 
     """
     Рассчитать конечные точки для каждого робота
     """
     def process(self, field: field.Field):
+        if self.state == States.DEBUG:
+            return self.debug(field)
+        elif self.state == States.DEFENCE:
+            return self.defence(field)
+
+    def debug(self, field: field.Field):
         waypoints = [None]*const.TEAM_ROBOTS_MAX_COUNT
-        for i in range(1, 6):
+        for i in range(6):
+            bbotpos = field.b_team[i].getPos()
+            ybotpos = field.y_team[i].getPos()
+            pos = aux.point_on_line(bbotpos, aux.Point(4500, 0), 300)
+            
+            dpos = bbotpos - ybotpos
+            angle = math.atan2(dpos.y, dpos.x)
+
+            waypoint = wp.Waypoint(pos, angle, wp.WType.ENDPOINT)
+            waypoints[i] = waypoint
+        return waypoints
+
+    def defence(self, field: field.Field):
+        waypoints = [None]*const.TEAM_ROBOTS_MAX_COUNT
+        for i in range(6):
             bbotpos = field.b_team[i].getPos()
             ybotpos = field.y_team[i].getPos()
             pos = aux.point_on_line(bbotpos, aux.Point(4500, 0), 300)
