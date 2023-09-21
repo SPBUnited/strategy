ce15da55a5af97939465fed76c8e259a0cb5e75d
Arseniy <yarmolinskiyam@gmail.com>
2023-09-21 12:26:58 +0300

    velocity estimation

--- bridge/processors/entity.py
+++ bridge/processors/entity.py
@@ -12,6 +12,22 @@
 import math
 import bridge.processors.auxiliary as aux
 
+class FOD:
+    def __init__(self, T, dT):
+        self.T = T
+        self.dT = dT
+        self.I = 0
+        self.out = 0
+
+    def process(self, x):
+        err = x - self.I
+        self.out = err/self.T
+        self.I += self.out * self.dT
+        return self.out
+
+    def getVal(self):
+        return self.out
+
 class Entity:
 
     """
@@ -22,9 +38,16 @@ class Entity:
     @param R Радиус объекта [м]
     """
     def __init__(self, pos, angle, R) -> None:
+        T = 0.2
+        dT = 1/30
+
         self.pos = pos
         self.vel = aux.Point(0, 0)
+        self.velFx = FOD(T, dT)
+        self.velFy = FOD(T, dT)
         self.acc = aux.Point(0, 0)
+        self.accFx = FOD(T, dT)
+        self.accFy = FOD(T, dT)
         self.angle = angle
         self.R = R
 
@@ -37,6 +60,10 @@ class Entity:
     def update(self, pos, angle):
         self.pos = pos
         self.angle = angle
+        self.vel.x = self.velFx.process(self.pos.x)
+        self.vel.y = self.velFy.process(self.pos.y)
+        self.acc.x = self.accFx.process(self.vel.x)
+        self.acc.y = self.accFy.process(self.vel.y)
 
     """ Геттер положения """
     def getPos(self) -> aux.Point:
