32ca00a3691d106dfec8e7def6b3be13a7f95367
Mikhail Lipkovich <lipkovich.mikhail@gmail.com>
2023-09-24 16:07:51 +0300

    Use strategy-bridge as dependency

--- bridge/processors/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_processor import BaseProcessor
-from .vision_detections_collector import VisionDetectionsCollector
-from .robot_commands_sender import RobotCommandsSender
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from bridge.common import config
-from bridge.processors import VisionDetectionsCollector, RobotCommandsSender
-from bridge.processors.python_controller import MatlabController
-from bridge.processors.referee_commands_collector import RefereeCommandsCollector
-from bridge.runner import Runner
+from strategy_bridge.common import config
+from strategy_bridge.processors import VisionDetectionsCollector, RobotCommandsSender
+from strategy_bridge.processors.referee_commands_collector import RefereeCommandsCollector
+from strategy_bridge.runner import Runner
 
+from bridge.processors.python_controller import MatlabController
 
 if __name__ == '__main__':
 
-    config.init_logging()
+    config.init_logging("./logs")
 
     # TODO: Move list of processors to config
     processors = [
