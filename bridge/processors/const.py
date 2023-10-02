c253db20d42b16690fbc298ba21a1d97aca0363e
ssl-vision <dogon.dogon@mail.ru>
2023-10-02 17:08:04 +0300

    Fix speed coefs

--- bridge/processors/const.py
+++ bridge/processors/const.py
@@ -10,7 +10,7 @@ CAMERAS_COUNT: int = 4
 MAX_BALLS_IN_CAMERA: int = 64
 MAX_BALLS_IN_FIELD: int = CAMERAS_COUNT * MAX_BALLS_IN_CAMERA
 BALL_PACKET_SIZE: int = 3
-SIDE = -1 #1 if our goal on plus coords on X. -1 if our goal on minus coords on X.
+SIDE = 1 #1 if our goal on plus coords on X. -1 if our goal on minus coords on X.
 
 ROBOTS_MAX_COUNT: int = 32
 TEAM_ROBOTS_MAX_COUNT: int = ROBOTS_MAX_COUNT // 2
@@ -25,14 +25,14 @@ GEOMETRY_PACKET_SIZE: int = 2
 Ts = 0.020 # 20ms
 
 # ROBOT SETTING CONSTS
-MAX_SPEED = 50
+MAX_SPEED = 150
 MAX_SPEED_R = 50
 ACCELERATION = 3
 BASE_KICKER_VOLTAGE = 7.0
 
 R_KP = 7
 R_KD = 0
-KP = 0.05
+KP = 0.2
 ##################################################
 # GEOMETRY CONSTS
 
