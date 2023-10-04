6b7590b50bf9fe993e130707162dd52698d9b714
Arseniy <yarmolinskiyam@gmail.com>
2023-10-04 21:27:18 +0300

    command sink 2

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ from strategy_bridge.processors.referee_commands_collector import RefereeCommand
 from strategy_bridge.runner import Runner
 
 from bridge.processors.python_controller import MatlabController
+from bridge.processors.robot_command_sink import Sink, CommandSink
 
 if __name__ == '__main__':
 
     config.init_logging("./logs")
 
+    msink = Sink()
+
     # TODO: Move list of processors to config
     processors = [
         VisionDetectionsCollector(processing_pause=0.001),
         RefereeCommandsCollector(processing_pause=0.1),
-        MatlabController(),
-        RobotCommandsSender(processing_pause=0.001)
+        MatlabController(our_color='y', sink=msink),
+        # CommandSink(processing_pause = 0.01, sink=msink),
+        RobotCommandsSender(processing_pause=0.01)
     ]
 
     runner = Runner(processors=processors)
