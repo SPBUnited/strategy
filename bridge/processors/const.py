a972ecb20e7c4af9e9b587c7df451b2a992a89f8
AlexM <dogon.dogon@mail.ru>
2023-10-05 15:20:22 +0300

    Revert "Add attack"
    This reverts commit a88724e12cc4cde7d68aab4e79d29ebbac91dfc8.

--- bridge/processors/const.py
+++ bridge/processors/const.py
@@ -2,15 +2,15 @@ import bridge.processors.auxiliary as aux
 
 ##################################################
 # GAME SETTING CONSTS
+OUR_COLOR = 'b'
 GK = 5
 ENEMY_GK = 5
-IS_SIMULATOR_USED = 1
+IS_SIMULATOR_USED = 1 #ALWAYS ON
 CAMERAS_COUNT: int = 4
 MAX_BALLS_IN_CAMERA: int = 64
 MAX_BALLS_IN_FIELD: int = CAMERAS_COUNT * MAX_BALLS_IN_CAMERA
 BALL_PACKET_SIZE: int = 3
-SIDE = 1 #1 if our goal on plus coords on X. -1 if our goal on minus coords on X.
-KOEFF_NAGLO: float = 0.5
+SIDE = -1 #1 if our goal on plus coords on X. -1 if our goal on minus coords on X.
 
 ROBOTS_MAX_COUNT: int = 32
 TEAM_ROBOTS_MAX_COUNT: int = ROBOTS_MAX_COUNT // 2
@@ -21,19 +21,30 @@ GEOMETRY_PACKET_SIZE: int = 2
 ##################################################
 
 ##################################################
+# CONTROL CONSTS
+Ts = 0.020 # 20ms
+
 # ROBOT SETTING CONSTS
-MAX_SPEED = 30
-MAX_SPEED_R = 50
+MAX_SPEED = 1500
+MAX_ANG_SPEED = 4
 ACCELERATION = 3
 BASE_KICKER_VOLTAGE = 7.0
 
 R_KP = 7
 R_KD = 0
-KP = 0.05
+KP = 0.2
 ##################################################
 # GEOMETRY CONSTS
 
 BALL_R = 0.05
 ROBOT_R = 0.2
 GRAVEYARD_POS = aux.Point(-10000, 0)
-GOAL_X = 3000
+GOAL_DX = 4500
+GOAL_DY = 500
+
+B_GOAL_C = aux.Point(-GOAL_DX, 0)
+B_GOAL_U = aux.Point(-GOAL_DX, GOAL_DY)
+B_GOAL_D = aux.Point(-GOAL_DX, -GOAL_DY)
+Y_GOAL_C = aux.Point(GOAL_DX, 0)
+Y_GOAL_U = aux.Point(GOAL_DX, GOAL_DY)
+Y_GOAL_D = aux.Point(GOAL_DX, -GOAL_DY)
