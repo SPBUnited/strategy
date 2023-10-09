9e1cc12833077882c1bef215147914f5ed811d12
Arseniy <yarmolinskiyam@gmail.com>
2023-10-09 06:45:43 +0300

    .

--- bridge/processors/const.py
+++ bridge/processors/const.py
@@ -4,7 +4,7 @@ import bridge.processors.auxiliary as aux
 # GAME SETTING CONSTS
 GK = 1
 PENALTY_KICKER = 2
-ENEMY_GK = 1
+ENEMY_GK = 5
 IS_SIMULATOR_USED = True
 CAMERAS_COUNT: int = 4
 MAX_BALLS_IN_CAMERA: int = 64
@@ -20,8 +20,11 @@ ROBOT_TEAM_PACKET_SIZE: int = SINGLE_ROBOT_PACKET_SIZE * TEAM_ROBOTS_MAX_COUNT
 
 GEOMETRY_PACKET_SIZE: int = 2
 
+DEBUG_ID = 1
+DEBUG_CTRL = 9
 CONTROL_MAPPING = \
 {
+    # DEBUG_ID: DEBUG_CTRL
     0: 0,
     1: 1,
     2: 2,
@@ -29,7 +32,6 @@ CONTROL_MAPPING = \
     4: 4,
     5: 5
 }
-
 
 for i in range(TEAM_ROBOTS_MAX_COUNT):
     try:
@@ -42,7 +44,7 @@ TOPIC_SINK = "control-sink"
 
 ##################################################
 # CONTROL CONSTS
-Ts = 0.035 # s
+Ts = 0.02 # s
 
 # ROBOT SETTING CONSTS
 # MAX_SPEED = 100
@@ -66,9 +68,9 @@ GK_PEN_KICKOUT_SPEED = 500
 BALL_R = 0.05
 ROBOT_R = 0.2
 GRAVEYARD_POS = aux.Point(-10000, 0)
-GOAL_DX = 4500
-GOAL_DY = 1000
-GOAL_PEN = 500
+GOAL_DX = -1250
+GOAL_DY = 800
+GOAL_PEN = -800
 GOAL_BOUND_OFFSET = 100
 GOAL_WALLLINE_OFFSET = 1800
 GOAL_WALL_ROBOT_SEPARATION = 150
