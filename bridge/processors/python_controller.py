5d39726b6e5dd6ff8a40afba11be144b12e0f943
AlexM-dev <dogon.dogon@mail.ru>
2023-11-03 19:38:46 +0300

    remove strategy and game controller

--- bridge/processors/python_controller.py
+++ bridge/processors/python_controller.py
@@ -114,42 +114,6 @@ class SSLController(BaseProcessor):
             
             # self.strategy.changeGameState(strategy.GameStates.RUN, 0)
 
-            curCmd = self.get_last_referee_command()
-            if curCmd.state == 0:
-                self.count_halt_cmd += 1
-            else:
-                self.count_halt_cmd = 0
-                if curCmd.state == 1:
-                    self.strategy.changeGameState(strategy.GameStates.STOP, curCmd.commandForTeam)
-                elif curCmd.state == 2:
-                    self.strategy.changeGameState(strategy.GameStates.RUN, curCmd.commandForTeam)
-                elif curCmd.state == 3:
-                    self.strategy.changeGameState(strategy.GameStates.TIMEOUT, curCmd.commandForTeam)
-                elif curCmd.state == 4:
-                    self.strategy.changeGameState(strategy.GameStates.HALT, curCmd.commandForTeam)
-                    print("End game")
-                elif curCmd.state == 5:
-                    self.strategy.changeGameState(strategy.GameStates.PREPARE_KICKOFF, curCmd.commandForTeam)
-                elif curCmd.state == 6:
-                    self.strategy.changeGameState(strategy.GameStates.KICKOFF, curCmd.commandForTeam)
-                elif curCmd.state == 7:
-                    self.strategy.changeGameState(strategy.GameStates.PREPARE_PENALTY, curCmd.commandForTeam)
-                elif curCmd.state == 8:
-                    self.strategy.changeGameState(strategy.GameStates.PENALTY, curCmd.commandForTeam)
-                elif curCmd.state == 9:
-                    self.strategy.changeGameState(strategy.GameStates.FREE_KICK, curCmd.commandForTeam)
-                elif curCmd.state == 10:
-                    self.strategy.changeGameState(strategy.GameStates.HALT, curCmd.commandForTeam)
-                    print("Uknown command 10")
-                elif curCmd.state == 11:
-                    self.strategy.changeGameState(strategy.GameStates.BALL_PLACMENT, curCmd.commandForTeam)
-                
-            if self.count_halt_cmd > 10:
-                self.strategy.changeGameState(strategy.GameStates.HALT, curCmd.commandForTeam)
-
-            # self.strategy.changeGameState(strategy.GameStates.PREPARE_KICKOFF, 0)
-
-
             # TODO: Barrier states
             for robot in detection.robots_blue:
                 if time.time() - self.field.b_team[robot.robot_id].last_update() > 0.3:
