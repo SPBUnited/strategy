b8958f43969518cecf4e37ac56e3bd217d22a5c7
Alexandr Mescheryakov <56480488+AlexM-dev@users.noreply.github.com>
2024-10-24 12:19:05 +0300

    Revert "Saveliytest"

--- bridge/processors/robot_command_sink.py
+++ bridge/processors/robot_command_sink.py
@@ -1,178 +1,107 @@
-"""
-Модуль-прослойка между стратегией и отправкой пакетов на роботов
-"""
-
-import struct
+import asyncio
+import logging
 import typing
-from time import time
+import struct
 
 import attr
-from strategy_bridge.bus import DataBus, DataReader, DataWriter
+
+from strategy_bridge.bus import DataReader, DataWriter
 from strategy_bridge.common import config
+from strategy_bridge.model.referee import RefereeCommand
 from strategy_bridge.processors import BaseProcessor
+from strategy_bridge.bus import DataBus
+from strategy_bridge.utils.debugger import debugger
+from strategy_bridge.pb.messages_robocup_ssl_wrapper_pb2 import SSL_WrapperPacket
 
-from bridge import const
-from bridge.auxiliary import aux, rbt
-
+import bridge.processors.robot as robot
+import bridge.processors.const as const
 
 @attr.s(auto_attribs=True)
 class CommandSink(BaseProcessor):
-    """
-    Прослойка между стратегией и отправкой пакетов на роботов
-    """
 
-    processing_pause: typing.Optional[float] = 0.01
-    reduce_pause_on_process_time: bool = False
+    processing_pause: typing.Optional[int] = 0.01
     max_commands_to_persist: int = 20
     commands_sink_reader: DataReader = attr.ib(init=False)
     commands_writer: DataWriter = attr.ib(init=False)
 
-    b_control_team = [
-        rbt.Robot(aux.GRAVEYARD_POS, 0, const.ROBOT_R, const.Color.BLUE, i, 0) for i in range(const.TEAM_ROBOTS_MAX_COUNT)
-    ]
-    y_control_team = [
-        rbt.Robot(aux.GRAVEYARD_POS, 0, const.ROBOT_R, const.Color.YELLOW, i, 0) for i in range(const.TEAM_ROBOTS_MAX_COUNT)
-    ]
+    b_control_team = [robot.Robot(const.GRAVEYARD_POS, 0, const.ROBOT_R, 'b', i, 0) for i in range(const.TEAM_ROBOTS_MAX_COUNT)]
+    y_control_team = [robot.Robot(const.GRAVEYARD_POS, 0, const.ROBOT_R, 'y', i, 0) for i in range(const.TEAM_ROBOTS_MAX_COUNT)]
+
 
     def initialize(self, data_bus: DataBus) -> None:
-        """
-        Инициализация
-        """
         super(CommandSink, self).initialize(data_bus)
         self.commands_sink_reader = DataReader(data_bus, const.TOPIC_SINK, 20)
         self.commands_writer = DataWriter(data_bus, config.ROBOT_COMMANDS_TOPIC, self.max_commands_to_persist)
 
     def process(self) -> None:
-        """
-        Метод обратного вызова процесса
-        """
-
+        
         cmds = self.commands_sink_reader.read_new()
 
-        if cmds is None:
-            return
-
-        # if len(cmds) > 0:
-        #     print(len(cmds), "total delay:", time() - cmds[0].content.last_update())
-
         for cmd in cmds:
-            r: rbt.Robot = cmd.content
-            if not r.is_used():
+            r = cmd.content
+            rId = r.rId
+            ctrlId = r.ctrlId
+
+            if ctrlId == None:
                 continue
-            ctrl_id = r.ctrl_id
 
-            if ctrl_id is None:
-                continue
-
-            if r.color == const.Color.BLUE:
-                self.b_control_team[ctrl_id].copy_control_fields(r)
-            elif r.color == const.Color.YELLOW:
-                self.y_control_team[ctrl_id].copy_control_fields(r)
-                # self.y_control_team[ctrl_id].used(1)
-
+            if r.color == 'b':
+                self.b_control_team[ctrlId].copy_control_fields(r)
+            elif r.color == 'y':
+                self.y_control_team[ctrlId].copy_control_fields(r)
+        
         rules = self.get_rules()
 
         self.commands_writer.write(rules)
 
-    def get_rules(self) -> bytes:
+    def get_rules(self):
         """
         Сформировать массив команд для отправки на роботов
         """
-        rules: list[float] = []
+        rules = []
 
-        if const.IS_SIMULATOR_USED:
-            for i in range(const.TEAM_ROBOTS_MAX_COUNT):
-                if not const.IS_DRIBBLER_USED:
-                    self.b_control_team[i].dribbler_enable_ = 0
-                    self.b_control_team[i].dribbler_speed_ = 0
+        for i in range(const.TEAM_ROBOTS_MAX_COUNT):
+            if abs(self.b_control_team[i].speedX) < 1.5:
+                self.b_control_team[i].speedX = 0
+            if abs(self.b_control_team[i].speedY) < 1.5:
+                self.b_control_team[i].speedY = 0
+            if abs(self.b_control_team[i].speedR) < 1.5:
+                self.b_control_team[i].speedR = 0
+            rules.append(0)
+            rules.append(self.b_control_team[i].speedX)
+            rules.append(self.b_control_team[i].speedY)
+            rules.append(self.b_control_team[i].speedR)
+            rules.append(self.b_control_team[i].kickForward)
+            rules.append(self.b_control_team[i].kickUp)
+            rules.append(self.b_control_team[i].autoKick)
+            rules.append(self.b_control_team[i].kickerVoltage)
+            rules.append(self.b_control_team[i].dribblerEnable)
+            rules.append(self.b_control_team[i].speedDribbler)
+            rules.append(self.b_control_team[i].kickerChargeEnable)
+            rules.append(self.b_control_team[i].beep)            
+            rules.append(0)
+        for i in range(const.TEAM_ROBOTS_MAX_COUNT):
+            if abs(self.y_control_team[i].speedX) < 1.5:
+                self.y_control_team[i].speedX = 0
+            if abs(self.y_control_team[i].speedY) < 1.5:
+                self.y_control_team[i].speedY = 0
+            if abs(self.y_control_team[i].speedR) < 1.5:
+                self.y_control_team[i].speedR = 0
+            rules.append(0)
+            rules.append(self.y_control_team[i].speedX)
+            rules.append(self.y_control_team[i].speedY)
+            rules.append(self.y_control_team[i].speedR)
+            rules.append(self.y_control_team[i].kickForward)
+            rules.append(self.y_control_team[i].kickUp)
+            rules.append(self.y_control_team[i].autoKick)
+            rules.append(self.y_control_team[i].kickerVoltage)
+            rules.append(self.y_control_team[i].dribblerEnable)
+            rules.append(self.y_control_team[i].speedDribbler)
+            rules.append(self.y_control_team[i].kickerChargeEnable)
+            rules.append(self.y_control_team[i].beep)            
+            rules.append(0)
 
-                if abs(self.b_control_team[i].speed_x) < 1:
-                    self.b_control_team[i].speed_x = 0
-                if abs(self.b_control_team[i].speed_y) < 1:
-                    self.b_control_team[i].speed_y = 0
-                if abs(self.b_control_team[i].speed_r) < 1:
-                    self.b_control_team[i].speed_r = 0
-                rules.append(0)
-                rules.append(self.b_control_team[i].speed_x)
-                rules.append(self.b_control_team[i].speed_y)
-                rules.append(self.b_control_team[i].speed_r)
-                rules.append(self.b_control_team[i].kick_up_)
-                rules.append(self.b_control_team[i].kick_forward_)
-                rules.append(self.b_control_team[i].auto_kick_)
-                rules.append(self.b_control_team[i].kicker_voltage_ // 2)
-                rules.append(self.b_control_team[i].dribbler_enable_)
-                rules.append(self.b_control_team[i].dribbler_speed_)
-                rules.append(self.b_control_team[i].kicker_charge_enable_)
-                rules.append(self.b_control_team[i].beep)
-                rules.append(0)
-
-            for i in range(const.TEAM_ROBOTS_MAX_COUNT):
-                if not const.IS_DRIBBLER_USED:
-                    self.y_control_team[i].dribbler_enable_ = 0
-                    self.y_control_team[i].dribbler_speed_ = 0
-                if abs(self.y_control_team[i].speed_x) < 1:
-                    self.y_control_team[i].speed_x = 0
-                if abs(self.y_control_team[i].speed_y) < 1:
-                    self.y_control_team[i].speed_y = 0
-                if abs(self.y_control_team[i].speed_r) < 1:
-                    self.y_control_team[i].speed_r = 0
-                rules.append(0)
-                rules.append(self.y_control_team[i].speed_x)
-                rules.append(self.y_control_team[i].speed_y)
-                rules.append(self.y_control_team[i].speed_r)
-                rules.append(self.y_control_team[i].kick_up_)
-                rules.append(self.y_control_team[i].kick_forward_)
-                rules.append(self.y_control_team[i].auto_kick_)
-                rules.append(self.y_control_team[i].kicker_voltage_ // 2)
-                rules.append(self.y_control_team[i].dribbler_enable_)
-                rules.append(self.y_control_team[i].dribbler_speed_)
-                rules.append(self.y_control_team[i].kicker_charge_enable_)
-                rules.append(self.y_control_team[i].beep)
-                rules.append(0)
-        else:
-            for i in range(const.TEAM_ROBOTS_MAX_COUNT):
-                control_team = self.y_control_team if self.y_control_team[i].is_used() else self.b_control_team
-
-                if self.y_control_team[i].is_used():
-                    pass
-                elif self.b_control_team[i].is_used():
-                    pass
-                else:
-                    for _ in range(13):
-                        rules.append(0)
-                    continue
-
-                if not const.IS_DRIBBLER_USED:
-                    if round(time() * 2) % 10 == 0:
-                        control_team[i].dribbler_enable_ = 1
-                        control_team[i].dribbler_speed_ = 1
-                    else:
-                        control_team[i].dribbler_enable_ = 0
-                        control_team[i].dribbler_speed_ = 0
-
-                if abs(control_team[i].speed_x) < 1:
-                    control_team[i].speed_x = 0
-                if abs(control_team[i].speed_y) < 1:
-                    control_team[i].speed_y = 0
-                if abs(control_team[i].speed_r) < 1:
-                    control_team[i].speed_r = 0
-                rules.append(0)
-                rules.append(control_team[i].speed_x)
-                rules.append(control_team[i].speed_y)
-                rules.append(control_team[i].speed_r)
-                rules.append(control_team[i].kick_up_)
-                rules.append(control_team[i].kick_forward_)
-                rules.append(control_team[i].auto_kick_)
-                rules.append(control_team[i].kicker_voltage_)
-                rules.append(control_team[i].dribbler_enable_)
-                rules.append(control_team[i].dribbler_speed_)
-                rules.append(control_team[i].kicker_charge_enable_)
-                rules.append(control_team[i].beep)
-                rules.append(0)
-            for _ in range(const.TEAM_ROBOTS_MAX_COUNT):
-                for _ in range(13):
-                    rules.append(0)
-
-        # rules = [15] * 13 * 32
+        #rules = [15] * 15 * 32
         b = bytes()
-        return b.join((struct.pack("d", rule) for rule in rules))
+        rules = b.join((struct.pack('d', rule) for rule in rules))
+        return rules
--- bridge/processors/tau.py
+++ bridge/processors/tau.py
@@ -3,158 +3,135 @@
 """
 
 import math
+import bridge.processors.auxiliary as aux
 from enum import Enum, auto
-
-from bridge.auxiliary import aux
-
 
 class FOD:
     """
     Реальное дифференцирующее звено первого порядка
     """
-
-    def __init__(self, T: float, Ts: float, is_angle: bool = False) -> None:
+    def __init__(self, T, dT, is_angle = False):
         """
         Конструктор
 
         T - постоянная времени ФНЧ
         dT - период квантования
         """
-        self._t = T
-        self._ts = Ts
-        self._int = 0.0
-        self._out = 0.0
+        self._T = T
+        self._dT = dT
+        self._I = 0
+        self._out = 0
         self._is_angle = is_angle
 
-    def process(self, x: float) -> float:
+    def process(self, x):
         """
         Рассчитать и получить следующее значение выхода звена
-
+        
         ВЫЗЫВАТЬ РАЗ В ПЕРИОД КВАНТВАНИЯ
 
         x - новое значение входа
         """
-        err = x - self._int
+        err = x - self._I
         if self._is_angle:
+            # print(err, x, self.I)
             if err > math.pi:
-                err -= 2 * math.pi
-                self._int += 2 * math.pi
+                err -= 2*math.pi
+                self._I += 2*math.pi
             elif err < -math.pi:
-                err += 2 * math.pi
-                self._int -= 2 * math.pi
-        self._out = err / self._t
-        self._int += self._out * self._ts
+                err += 2*math.pi
+                self._I -= 2*math.pi
+        self._out = err/self._T
+        self._I += self._out * self._dT
         return self._out
 
-    def get_val(self) -> float:
+    def getVal(self):
         """
         Получить последнее значение выхода звена без расчета
         """
         return self._out
-
 
 class FOLP:
     """
     Фильтр низких частот первого порядка
     """
-
-    def __init__(self, T: float, Ts: float) -> None:
+    def __init__(self, T, dT):
         """
         Конструктор
 
         T - постоянная времени ФНЧ
         dT - период квантования
         """
-        self._t = T
-        self._ts = Ts
-        self._int = 0.0
-        self._out = 0.0
+        self._T = T
+        self._dT = dT
+        self._I = 0
+        self._out = 0
 
-    def process(self, x: float) -> float:
+    def process(self, x):
         """
         Рассчитать и получить следующее значение выхода звена
-
+        
         ВЫЗЫВАТЬ РАЗ В ПЕРИОД КВАНТВАНИЯ
 
         x - новое значение входа
         """
         err = x - self._out
-        self._int += err * self._ts
-        self._out = self._int / self._t
+        self._I += err * self._dT
+        self._out = self._I/self._T
         return self._out
 
-    def get_val(self) -> float:
+    def getVal(self):
         """
         Получить последнее значение выхода звена без расчета
         """
         return self._out
 
-
-class Integrator:
+class Integrator():
     """
     Интегратор
     """
-
-    def __init__(self, Ts: float) -> None:
+    def __init__(self, dT):
         """
         Конструктор
 
         dT - период квантования
         """
-        self._ts = Ts
-        self._int = 0.0
-        self._out = 0.0
+        self._dT = dT
+        self._I = 0
+        self._out = 0
+    
+    def reset(self):
+        self._I = 0
 
-    def reset(self) -> None:
-        """
-        Сбросить значение интегратора
-        """
-        self._int = 0
-
-    def process(self, x: float) -> float:
+    def process(self, x):
         """
         Рассчитать и получить следующее значение выхода звена
-
+        
         ВЫЗЫВАТЬ РАЗ В ПЕРИОД КВАНТВАНИЯ
 
         x - новое значение входа
         """
-        self._int += x * self._ts
-        self._out = self._int
+        self._I += x * self._dT
+        self._out = self._I
         return self._out
 
-    def get_val(self) -> float:
+    def getVal(self):
         """
         Получить последнее значение выхода звена без расчета
         """
         return self._out
 
-
 class Mode(Enum):
-    """
-    Названия наборов коэффициентов регулятора
-    """
-
     NORMAL = 0
     SOFT = auto()
 
-
-class PISD:
+class PISD():
     """
     Пропорционально-скользяще-интегральный регулятор
 
     (В отличие от ПИД беред производную от скорости изменения регулируемой
     величины, а не ошибки)
     """
-
-    def __init__(
-        self,
-        dT: float,
-        gain: list[float],
-        kd: list[float],
-        ki: list[float],
-        max_out: list[float],
-    ) -> None:
+    def __init__(self, dT, gain, kd, ki, max_out) -> None:
         """
         Конструктор
 
@@ -168,76 +145,56 @@ class PISD:
         self.__kd = kd
         self.__ki = ki
         self.__max_out = max_out
-        self.__int = Integrator(dT)
-        self.__out = 0.0
+        self.__I = Integrator(dT)
+        self.__out = 0
         self.__mode = Mode.NORMAL
-
-    def select_mode(self, mode: Mode) -> None:
-        """
-        Выбрать набор коэффициентов регулятора
-        """
+    
+    def select_mode(self, mode: Mode):
         self.__mode = mode
-        self.__int.reset()
+        self.__I.reset()
+    
+    def __get_gains(self):
+        return (self.__gain[self.__mode.value], self.__kd[self.__mode.value], self.__ki[self.__mode.value], self.__max_out[self.__mode.value])
 
-    def __get_gains(self) -> tuple[float, float, float, float]:
-        """
-        Получить коэффициенты регулятора
-        """
-        return (
-            self.__gain[self.__mode.value],
-            self.__kd[self.__mode.value],
-            self.__ki[self.__mode.value],
-            self.__max_out[self.__mode.value],
-        )
-
-    def process(self, xerr: float, x_i: float) -> float:
+    def process(self, xerr, x_i):
         """
         Рассчитать следующий тик регулятора
         """
-        gain, k_d, k_i, max_out = self.__get_gains()
+        gain, kd, ki, max_out = self.__get_gains()
+        # print(gain, kd, ki, max_out)
 
-        s = xerr + k_d * x_i + k_i * self.__int.get_val()
+        s = xerr + kd*x_i + ki*self.__I.getVal()
         u = gain * s
 
-        u_clipped = aux.minmax(u, max_out)
+        u_clipped = aux.minmax(u, -max_out, max_out)
 
         if u != u_clipped:
-            self.__int.process(xerr + k_d * x_i)
+            self.__I.process(xerr + kd*x_i)
 
         self.__out = u_clipped
         return self.__out
 
-    def get_val(self) -> float:
+    def getVal(self):
         """
         Получить последнее значение выхода звена без расчета
         """
         return self.__out
 
-
-class RateLimiter:
+class RateLimiter():
     """
     Ограничитель скорости роста
     """
-
-    def __init__(self, Ts: float, max_der: float) -> None:
-        """
-        Конструктор
-        """
-        self.__out = 0.0
-        self.__int = Integrator(Ts)
-        self.__k = 1 / Ts
+    def __init__(self, Ts, max_der) -> None:
+        self.__out = 0
+        self.__I = Integrator(Ts)
+        self.__k = 1/Ts
         self.__max_der = max_der
-
-    def process(self, x: float) -> float:
-        """
-        Рассчитать следующий тик звена
-        """
-        u = aux.minmax(self.__k * (x - self.__out), self.__max_der)
-        self.__out = self.__int.process(u)
+    
+    def process(self, x):
+        u = aux.minmax(self.__k * (x - self.__out), -self.__max_der, self.__max_der)
+        self.__out = self.__I.process(u)
         return self.__out
 
-    def get_val(self) -> float:
-        """
-        Получить последнее значение выхода звена без расчета
-        """
+    def getVal(self):
         return self.__out
+
